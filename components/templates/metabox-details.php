4ba3aa2fb410df8426be0fca4734212dc2971f61
Casey Bisson <casey.bisson@gmail.com>
2014-02-02 19:14:22 -0800

    wip

---
+++ components/templates/metabox-details.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * html for the Details metabox.
+ * The context is the bDefinite_Admin class, where $post is set.
+ */
+
+$geo = bgeo()->get_geo( $tag->term_id, $tag->taxonomy );
+
+// create a default geo obgect if the above failed
+if( ! is_object( $geo ))
+{
+	$geo = (object) array(
+		'point' => NULL,
+		'point_lat' => NULL,
+		'point_lon' => NULL,
+		'bounds' => NULL,
+		'woeid' => NULL,
+	);
+}
+
+wp_localize_script( $this->id_base . '-admin', $this->id_base . '_term', (array) $geo );
+
+if ( isset( $this->instance['primary_term']->term_id ) )
+{
+	?>
+	(<a href="<?php echo get_edit_term_link( $tag->term_id, $tag->taxonomy ); ?>">edit map</a>)
+	<?php
+}
+
+?>
+<tr class="form-field">
+	<th scope="row" valign="top">
+		<label for="<?php echo $this->get_field_id( 'map' ); ?>">Map</label>
+	</th>
+	<td>
+		<div id="<?php echo $this->get_field_id( 'map' ); ?>"></div>
+	</td>
+</tr>
+
+<tr class="form-field">
+	<th scope="row" valign="top">
+		<label for="<?php echo $this->get_field_id( 'woeid' ); ?>">Bounds</label>
+	</th>
+	<td>
+		<input type="text" id="<?php echo $this->get_field_id( 'woeid' ); ?>" name="<?php echo $this->get_field_name( 'woeid' ); ?>" value="<?php echo( esc_attr( $geo->woeid ) ); ?>" size="10" />
+	</td>
+</tr>

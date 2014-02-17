daeed54c272fcca5297e2acc560f221d2ffab887
Casey Bisson <casey.bisson@gmail.com>
2014-02-17 15:56:02 -0800

    moving the php preamble out of the template and into the calling method, adding display of the WOEID and Wikipedia records

--- components/templates/metabox-details.php
+++ components/templates/metabox-details.php
@@ -3,28 +3,11 @@
  * html for the Details metabox.
  * The context is the bDefinite_Admin class, where $post is set.
  */
-
-$geo = bgeo()->get_geo( $tag->term_id, $tag->taxonomy );
-
-// create a default geo obgect if the above failed
-if( ! is_object( $geo ))
-{
-	$geo = (object) array(
-		'point' => NULL,
-		'point_lat' => NULL,
-		'point_lon' => NULL,
-		'bounds' => NULL,
-	);
-}
-
-
-wp_localize_script( $this->id_base . '-admin', $this->id_base . '_term', (array) $geo );
-
 ?>
 <table class="form-table">
 <?php
 
-if ( isset( $geo->point, $geo->point_lat, $geo->point_lon, $geo->bounds ) ) 
+if ( isset( $geo->point, $geo->point_lat, $geo->point_lon, $geo->bounds ) )
 {
 ?>
 	<tr class="form-field">
@@ -47,6 +30,12 @@ if ( isset( $geo->point, $geo->point_lat, $geo->point_lon, $geo->bounds ) )
 	</th>
 	<td>
 		<input type="text" id="<?php echo $this->get_field_id( 'woeid' ); ?>" name="<?php echo $this->get_field_name( 'woeid' ); ?>" value="<?php echo( esc_attr( $meta->woeid ) ); ?>" size="10" />
+		<?php
+		if ( isset( $meta->woeid_r ) )
+		{
+			echo '<p><pre>' . print_r( $meta->woeid_r, TRUE ) . '</pre></p>';
+		}
+		?>
 	</td>
 </tr>
 
@@ -56,6 +45,12 @@ if ( isset( $geo->point, $geo->point_lat, $geo->point_lon, $geo->bounds ) )
 	</th>
 	<td>
 		<input type="text" id="<?php echo $this->get_field_id( 'wikipedia' ); ?>" name="<?php echo $this->get_field_name( 'wikipedia' ); ?>" value="<?php echo( esc_attr( $meta->wikipedia ) ); ?>" size="10" />
+		<?php
+		if ( isset( $meta->wikipedia_r ) )
+		{
+			echo '<p><pre>' . print_r( $meta->wikipedia_r, TRUE ) . '</pre></p>';
+		}
+		?>
 	</td>
 </tr>
 </table>
